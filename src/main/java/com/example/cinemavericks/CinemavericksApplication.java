6d2aa40162711078237778f772dd544bcc06bbc3
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-03-04 15:06:50 +0000

    Intial commit

---
+++ src/main/java/com/example/cinemavericks/CinemavericksApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemavericks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemavericksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemavericksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cinemavericks/CinemavericksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemavericks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemavericksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
