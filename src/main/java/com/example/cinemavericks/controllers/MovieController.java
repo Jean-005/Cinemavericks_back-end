eecc999badf8dcd120cc092f17a56665496aea34
Monesha <moneshadhali@gmail.com>
2024-03-05 10:25:57 +0000

    bug fixed

--- src/main/java/com/example/cinemavericks/controllers/MovieController.java
+++ src/main/java/com/example/cinemavericks/controllers/MovieController.java
@@ -1,10 +1,50 @@
 package com.example.cinemavericks.controllers;
 
+import com.example.cinemavericks.models.Movie;
+import com.example.cinemavericks.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("movies")
 public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+    //Display all movies
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    //Display movie by id
+//    @GetMapping(value = "/{id}")
+//    public ResponseEntity<Movie> getMovieById(@RequestParam long movieId){
+//
+//    }
+
+
+    //Add new movie
+
+
+    //Edit details of a movie
+
+
+    //Show all reviews for a specific movie
+
+
+    //Ex:Show all movie lists that contain a movie
+
+    //Ex: Filter movies by rating
+
 
 }
--- src/main/java/com/example/cinemavericks/services/MovieListService.java
+++ src/main/java/com/example/cinemavericks/services/MovieListService.java
@@ -1,9 +1,11 @@
 package com.example.cinemavericks.services;
 
 import com.example.cinemavericks.repositories.MovieListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieListService {
+    @Autowired
     MovieListRepository movieListRepository;
 }
