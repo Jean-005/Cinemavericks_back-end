63da868ff5c06a793b4802ff59b8d602604c0b02
Leila Peltier <l_peltier@outlook.com>
2024-03-05 15:23:37 +0000

    Created POST review endpoint

--- src/main/java/com/example/cinemavericks/controllers/ReviewController.java
+++ src/main/java/com/example/cinemavericks/controllers/ReviewController.java
@@ -1,9 +1,28 @@
 package com.example.cinemavericks.controllers;
 
+import com.example.cinemavericks.models.Review;
+import com.example.cinemavericks.repositories.ReviewRepository;
+import com.example.cinemavericks.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("reviews")
+@RequestMapping("/reviews")
 public class ReviewController {
+    @Autowired
+    ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Review review){
+        Review newReview = reviewService.createReview(review);
+        return new ResponseEntity<>(newReview, HttpStatus.CREATED);
+    }
+
+
+
 }
--- src/main/java/com/example/cinemavericks/services/ReviewService.java
+++ src/main/java/com/example/cinemavericks/services/ReviewService.java
@@ -1,5 +1,6 @@
 package com.example.cinemavericks.services;
 
+import com.example.cinemavericks.models.Review;
 import com.example.cinemavericks.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
     @Autowired
     ReviewRepository reviewRepository;
+
+    public Review createReview(Review review){
+        return reviewRepository.save(review);
+    }
 }
