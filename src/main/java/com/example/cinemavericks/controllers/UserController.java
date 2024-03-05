49f7dd891909eaccad3daedf2dad32e4c56a06a8
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-03-05 10:19:45 +0000

    added get user by id endpoint

--- src/main/java/com/example/cinemavericks/controllers/UserController.java
+++ src/main/java/com/example/cinemavericks/controllers/UserController.java
@@ -5,12 +5,10 @@ import com.example.cinemavericks.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -23,5 +21,15 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> allUsers = userService.getAllUsers();
         return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id){
+        Optional<User> user = userService.getUserById(id);
+        if(user.isPresent()){
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+        
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/cinemavericks/services/UserService.java
+++ src/main/java/com/example/cinemavericks/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +15,9 @@ public class UserService {
 
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(long id){
+        return userRepository.findById(id);
     }
 }
