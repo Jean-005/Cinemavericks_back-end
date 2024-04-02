fac81560855f0348a20230dd8d4bccef51901260
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-04-02 15:00:00 +0100

    Added data to dataloader.

--- src/main/java/com/example/cinemavericks/components/DataLoader.java
+++ src/main/java/com/example/cinemavericks/components/DataLoader.java
@@ -40,6 +40,11 @@ public class DataLoader implements ApplicationRunner {
         User user4 = new User("Monesha");
         User user5 = new User("Not Zarrin");
         User user6 = new User("Tommy");
+        User user7 = new User("Hannah");
+        User user8 = new User("Ivan");
+        User user9 = new User("Jasmine");
+        User user10 = new User("Kai");
+        User user11 = new User("Lena");
 
         userRepository.save(user1);
         userRepository.save(user2);
@@ -47,6 +52,11 @@ public class DataLoader implements ApplicationRunner {
         userRepository.save(user4);
         userRepository.save(user5);
         userRepository.save(user6);
+        userRepository.save(user7);
+        userRepository.save(user8);
+        userRepository.save(user9);
+        userRepository.save(user10);
+        userRepository.save(user11);
 
         Genre action = new Genre(GenreEnum.ACTION);
         Genre adventure = new Genre(GenreEnum.ADVENTURE);
@@ -75,6 +85,10 @@ public class DataLoader implements ApplicationRunner {
         List<Genre> genres2 = new ArrayList<>();
         List<Genre> genres3 = new ArrayList<>();
         List<Genre> genres4 = new ArrayList<>();
+        List<Genre> genres13 = new ArrayList<>();
+        List<Genre> genres14 = new ArrayList<>();
+        List<Genre> genres15 = new ArrayList<>();
+        List<Genre> genres16 = new ArrayList<>();
 
         genres1.add(comedy);
         genres1.add(adventure);
@@ -82,11 +96,24 @@ public class DataLoader implements ApplicationRunner {
         genres2.add(action);
         genres3.add(romance);
         genres4.add(scifi);
+        genres13.add(anime);
+        genres13.add(fantasy);
+        genres14.add(historical);
+        genres14.add(thriller);
+        genres15.add(comedy);
+        genres15.add(action);
+        genres16.add(horror);
+        genres16.add(romance);
+
 
         Movie hoodWinked = new Movie("Hoodwinked", 2005 ,genres1,"Cory Edwards", 80);
         Movie whipLash = new Movie("Whiplash", 2014,genres2, "Damien Chazelle",  107);
         Movie thisIsMeNow = new Movie("This is Me Now", 2024,genres1,"Dave Mayers",  65);
         Movie scottPilgrimVsTheWorld = new Movie("Scott Pilgrim Vs The World", 2010,genres2,"Edgar Wright",  112);
+        Movie naruto = new Movie("Naruto: Shippuden", 2007, genres13, "Hayato Date", 500);
+        Movie gladiator = new Movie("Gladiator", 2000, genres14, "Ridley Scott", 155);
+        Movie anchorMan = new Movie("Anchorman: The Legend of Ron Burgundy", 2004, genres15, "Adam McKay", 94);
+        Movie scream = new Movie("Scream", 1996, genres16, "Wes Craven", 111);
 
         Movie movie1 = new Movie("The Shawshank Redemption", 1994,genres2,"Frank Darabont",  142);
         Movie movie2 = new Movie("The Godfather", 1972,genres2,"Francis Ford Coppola", 175);
@@ -113,15 +140,39 @@ public class DataLoader implements ApplicationRunner {
         movieRepository.save(movie8);
         movieRepository.save(movie9);
         movieRepository.save(movie10);
+        movieRepository.save(naruto);
+        movieRepository.save(gladiator);
+        movieRepository.save(anchorMan);
+        movieRepository.save(scream);
 
         Review review1 = new Review(user3, scottPilgrimVsTheWorld, "wow so cool","FILM SO GOOD WOW WOOOOOOO!", 5);
         Review review2 = new Review(user5, movie2, "Not Great","Hmmm, it was okay. Not the best but not bad.", 2.5);
         Review review3 = new Review(user6, movie2, "Don't even","Don't.", 1);
+        Review review4 = new Review(user7, movie1, "A classic", "One of the best movies of all time", 5);
+        Review review5 = new Review(user8, movie10, "Awesome!", "The special effects are amazing!", 4.5);
+        Review review6 = new Review(user9, naruto, "Great!", "I love anime!", 4);
+        Review review7 = new Review(user10, gladiator, "Good", "A bit too long but still a good watch", 3.5);
+        Review review8 = new Review(user11, anchorMan, "Okay...", "Not a big fan of comedies", 2.5);
+
 
 
         reviewRepository.save(review1);
         reviewRepository.save(review2);
         reviewRepository.save(review3);
+        reviewRepository.save(review4);
+        reviewRepository.save(review5);
+        reviewRepository.save(review6);
+        reviewRepository.save(review7);
+        reviewRepository.save(review8);
+
+        // Calculate the average rating for the movies with new reviews
+        movie1 = movieRepository.findById(1L).get();
+        movie1.calculateAverageRating();
+        movieRepository.save(movie1);
+
+        movie10 = movieRepository.findById(10L).get();
+        movie10.calculateAverageRating();
+        movieRepository.save(movie10);
 
         // Making movie object equal to its database counterpart that has a review attached to it
         movie2 = movieRepository.findById(6L).get();
@@ -152,6 +203,7 @@ public class DataLoader implements ApplicationRunner {
         movieList7.addMovie(movie6);
         movieList7.addMovie(movie10);
 
+
         movieListRepository.save(movieList1);
         movieListRepository.save(movieList2);
         movieListRepository.save(movieList3);
@@ -160,6 +212,22 @@ public class DataLoader implements ApplicationRunner {
         movieListRepository.save(movieList6);
         movieListRepository.save(movieList7);
 
+        MovieList movieList8 = new MovieList(user7, "Fav List");
+        movieList8.addMovie(movie1);
+        movieList8.addMovie(movie10);
+        movieListRepository.save(movieList8);
+
+        MovieList movieList9 = new MovieList(user8, "Best List");
+        movieList9.addMovie(movie10);
+        movieListRepository.save(movieList9);
+
+        MovieList movieList10 = new MovieList(user9, "Watch List");
+        movieList10.addMovie(naruto);
+        movieListRepository.save(movieList10);
+
+        MovieList movieList11 = new MovieList(user10, "Worst List");
+        movieList11.addMovie(gladiator);
+        movieListRepository.save(movieList11);
 
     }
 }
